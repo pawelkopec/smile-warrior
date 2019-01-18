6769fbf1a6078bf318c7cbede57e113fd1c93712
Kecz <bartek8597@wp.pl>
2019-01-18 11:50:26 +0100

    Adding module to load data set and divide it into train, test and validate sets and script to test that module

---
+++ Test_load.py
@@ -0,0 +1,18 @@
+#Test script to load data by function
+
+import sys
+sys.path.append("B:\STUDIA MATERIALY\Gradient\Rozpoznawanie usmiechow\Rozpoznawanie usmiechow program")
+from server import load_data
+
+print("Hello World")
+filename = 'smile_warrior_dataset.csv'
+
+X_train, Y_train, X_test, Y_test, X_validate, Y_validate = load_data(filename)
+
+print(X_train.shape, Y_train.shape)
+print(X_test.shape, Y_test.shape)
+print(X_validate.shape, Y_validate.shape)
+
+
+
+
