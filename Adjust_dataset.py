7d4127258f9a798e364863653b114344f74b52c3
Kecz <bartek8597@wp.pl>
2019-01-26 14:53:36 +0100

    Minor changes in names of files, adding a few 'prints' informing what is currently happening and updating README

--- Adjust_dataset.py
+++ Adjust_dataset.py
@@ -1,43 +1,26 @@
 #Script changing labels of emotions to 1 - smile, 0 - no smile and saving changed dataset to new csv file
 
-import pandas as pd
 
-data_set = pd.read_csv("fer2013.csv")
+def main():
+    import pandas as pd
 
-print("Before:")
-print(data_set.head(n=20))
+    HAPPINESS = 3
 
-print(data_set.shape)
+    data_set = pd.read_csv("fer2013.csv")
 
-#Counting size of train, test and validation sets
-training_sample = 0
-test_sample = 0
-validation_sample= 0
+    print("Adjusting dataset... ")
 
-print("Adjusting dataset... ")
-
-#Emotion with label 3 is happines to we change label to 1 for our smile detector, other emotions are changed to 0.
-for index,row in data_set.iterrows():
-    if (row['emotion'] == 3):
-        data_set.at[index, 'emotion'] = 1
-    else:
-        data_set.at[index, 'emotion'] = 0
-
-    if (row['Usage'] == 'Training'):
-        training_sample= training_sample + 1
-    elif (row['Usage'] == 'PublicTest'):
-        test_sample= test_sample + 1
-    elif (row['Usage'] == 'PrivateTest'):
-        validation_sample= validation_sample + 1
+    #Emotion with label 3 is happines to we change label to 1 for our smile detector, other emotions are changed to 0.
+    for index, row in data_set.iterrows():
+        if row['emotion'] == HAPPINESS:
+            data_set.at[index, 'emotion'] = 1
+        else:
+            data_set.at[index, 'emotion'] = 0
 
 
-print("Size of training data set: %d " % training_sample)         #28709
-print("Size of test data set: %d " % test_sample)                 #3589
-print("Size of validation data set: %d " % validation_sample)     #3589
+    data_set.to_csv('smile_warrior_dataset.csv', index=False)
+    print("Smile-Warrior dataset created in file 'smile_warrior_dataset.csv'")
 
-print("After:")
-print(data_set.head(n=20))
 
-data_set.to_csv('smile_warrior_dataset.csv', index=False)
-print("Smile-Warrior dataset created in file 'smile_warrior_dataset.csv'")
-
+if __name__ == "__main__":
+    main()
