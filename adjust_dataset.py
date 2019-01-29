48329c70f79ae833208b26f867d159fbc04facc3
Kecz <bartek8597@wp.pl>
2019-01-29 18:37:18 +0100

    Minor upgrades v5

--- adjust_dataset.py
+++ adjust_dataset.py
@@ -1,24 +1,40 @@
 # Script changing labels of emotions to 1 - smile, 0 - no smile and saving changed dataset to new csv file
 import pandas as pd
+import argparse
 
 HAPPINESS = 3
 
 
 def main():
 
-    data_set = pd.read_csv("fer2013.csv")
+    #data_set = pd.read_csv("fer2013.csv")
+
+    parser = argparse.ArgumentParser(description='Input and output csv file')
+
+    parser.add_argument('input', metavar="input csv file in form: filename.csv", type=argparse.FileType('r'),
+                        help='Input csv file which will be adjusted')
+
+    parser.add_argument('output', metavar="output csv file in form: filename.csv", type=argparse.FileType('w'),
+                        help='Output csv file which will be adjusted')
+
+    args = parser.parse_args()
+
+    data_set = pd.read_csv(args.input)
 
     print("Adjusting dataset... ")
 
-    # Emotion with label 3 is happines to we change label to 1 for our smile detector, other emotions are changed to 0.
+    # Emotion with label 3 is happiness to we change label to 1 for our smile detector, other emotions are changed to 0.
     for index, row in data_set.iterrows():
         if row['emotion'] == HAPPINESS:
             data_set.at[index, 'emotion'] = 1
         else:
             data_set.at[index, 'emotion'] = 0
 
-    data_set.to_csv('smile_warrior_dataset.csv', index=False)
-    print("Smile-Warrior dataset created in file 'smile_warrior_dataset.csv'")
+    data_set.to_csv(args.output, index=False)
+
+    # data_set.to_csv(smile_warrior_dataset.csv, index=False)
+
+    print("Smile-Warrior dataset created in file %s" % args.output)
 
 
 if __name__ == "__main__":
--- example.py
+++ example.py
@@ -1,6 +1,7 @@
 # Example how to load dataset by function from module and to show single picture
 
 import sys
+import argparse
 
 # You may need to execute line below:
 # sys.path.append("Your path to project file for example: B:/Gradient/Smile_warrior")
@@ -9,7 +10,25 @@ from data_server import *
 
 # You have to firstly create smile_warrior_dataset by running script 'Adjust_data_set' which creates that data_set from
 # fer2013 data set
+
+
+# With passing path to csv file from command line argument
+# =======================================================
+# parser = argparse.ArgumentParser(description='Input csv file')
+#
+# parser.add_argument('input', metavar="input csv file in form: filename.csv",
+#                     help='Input csv file with dataset')
+#
+# args = parser.parse_args()
+# filename = args.input
+# =========================================================
+
+
+# With manual path to csv file
+# =========================================================
 filename = 'smile_warrior_dataset.csv'
+# =========================================================
+
 
 X_train, Y_train, X_test, Y_test, X_validate, Y_validate = load_dataset(filename)
 
